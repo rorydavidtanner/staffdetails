82c813ef431518626b844dd0b4e2feec5c4d60a7
Rory Tanner <roryscoding@gmail.com>
2020-10-25 13:46:45 +1100

    write app code

--- app.js
+++ app.js
@@ -12,35 +12,106 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
+// User questions 
 
-const inputDataCollection = [
-    {type: 'input', name: 'employeeId', message: 'Identification Code of the Employee :'},
-    {type: 'input', name: 'employeeName', message: 'Name of the Employee : '},
-    {type: 'input', name: 'department', message: 'Department of the Employee :'},
-    {type: 'input', name: 'position', message: 'Position of the Employee :'},
-    {type: 'input', name: 'earningsType', message: 'Earning Type of Employee :'},
-    {type: 'input', name: 'workedHours', message: 'Worked hours of the Employee :'},
-    {type: 'input', name: 'age', message: 'Age of the Employee :'}
-  ]
-// and to create objects for each team member (using the correct classes as blueprints!)
+const userQuestions = [
+    {
+        type: "input", 
+        name: "employeeId", 
+        message: "What is your Employee ID :",
+    },
+    
+    {
+        type: "input", 
+        name: "employeeName", 
+        message: "What is your name : ",
+    },
+    
+    {
+        type: "input", 
+        name: "email", 
+        message: "What is your email :",
+    },
+    
+    {
+        type: "list", 
+        name: "role", 
+        message: "What is your role :", 
+        choices: ["Manager", "Engineer", "Intern"]
+    },
+    
+    {
+        type: "input", 
+        name: "officeNumber", 
+        message: "What is your office number?", 
+        when: function(answers){
+            return answers.role === "Manager";
+        },
+    },
+    
+    {
+        type: "input", 
+        name: "school", 
+        message: "Which school do you go to?",
+        when: function (answers) {
+            return answers.role === "Intern";
+        },
+    },
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    {
+        type: "input", 
+        name: "github", 
+        message: "What is your username for GitHub?",
+        when: function(answers){
+            return answers.role === "Engineer"; 
+        },
+    },
+    {
+        type: "confirm", 
+        name: "enterAgain", 
+        message: "Would you like to add another employee?",
+        default: true,
+    },
+  ];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+  // prompting user to add more eployees 
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+  async function createEmployees (employeeInput = []) {
+    try {
+        const { again, ...answers } = await inquirer.prompt(userQuestions);
+        const newEmployee = [...employeeInput, answers];
+        return again ? createEmployees(newEmployee) : newEmployee
+      } catch (err) {
+          throw err;
+      }
+  }
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+  async function init() {
+    try {
+        const employees = [];
+        const employeeData = await createEmployees();
+
+        employeeData.map((employee) => {
+            const { name, id, role, officeNumber, school, github } = employee;
+
+            if (role === "Manager") {
+                const newManager = new Manager(name, id, email, officeNumber);
+                employees.push(newManager);
+            } else if (role === "Engineer") {
+                const newEngineer = new Engineer(name, id, email, github);
+                employees.push(newEngineer);
+
+            } else {
+                const newIntern = new Intern(name, id, email, school);
+                employees.push(newIntern);
+            }
+        });
+
+        const renderEmployee = render(employees);
+        fs.writeFile(outputPath, renderEmployee, () => console.log("BAM! Successful!"));
+    }   catch (err) {
+        throw new Error(err);
+    }
+}
+
+init();
